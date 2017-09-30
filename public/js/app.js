08e7f64808d0e4caf7da62d30661c4c96c616c10
remiilekun <remilekun.salami@gmail.com>
2017-09-30 08:41:25 +0100

    fab

--- public/js/app.js
+++ public/js/app.js
@@ -755,6 +755,23 @@ module.exports = __webpack_require__(35);
 
 __webpack_require__(9);
 
+jQuery(document).ready(function () {
+    var offset = 250;
+    var duration = 500;
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.fbtt').fadeIn(duration);
+        } else {
+            jQuery('.fbtt').fadeOut(duration);
+        }
+    });
+    jQuery('.fbtt').click(function (event) {
+        event.preventDefault();
+        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    });
+});
+
 /***/ }),
 /* 9 */
 /***/ (function(module, exports, __webpack_require__) {
