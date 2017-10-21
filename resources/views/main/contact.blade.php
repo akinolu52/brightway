406f28bc24bc58abe08aa0c12a920d2a085094ee
remiilekun <remilekun.salami@gmail.com>
2017-10-21 13:44:18 +0100

    map update

--- resources/views/main/contact.blade.php
+++ resources/views/main/contact.blade.php
@@ -93,9 +93,9 @@
 
     <script>
         function initMap() {
-            var uluru = {lat: -25.363, lng: 131.044};
+            var uluru = {lat: 6.5532415, lng: 3.2649028};
             var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
+                zoom: 16,
                 center: uluru
             });
             var marker = new google.maps.Marker({
