51f3093fd5fd1cd311ea34105fa4bf4b115557a4
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2018-10-01 23:12:36 +0100

    homepage slider

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -362,7 +362,285 @@
 							style="z-index: 16; white-space: nowrap;text-transform:left;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer; font-family: 'Trirong', serif;">CLICK TO NEXT </div>
 					</li>
 					<!-- SLIDE  -->
-					<li data-index="rs-2980" data-transition="fadethroughdark" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="2000"  data-thumb="images/slider/slider3.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Warehousing" data-param1="03" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+					<li data-index="rs-2980" data-transition="fadethroughdark" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="2000"  data-thumb="images/slider/slider4.jpeg"  data-rotate="0"  data-saveperformance="off"  data-title="Logistics" data-param1="03" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+						<!-- MAIN IMAGE -->
+						<img src="images/slider/slider4.jpg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="3" class="rev-slidebg" data-no-retina>
+						<!-- LAYERS -->
+
+						<!-- LAYER NR. 7 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-tobggroup" 
+							 id="slide-2979-layer-1" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']" 
+										data-width="full"
+							data-height="full"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-basealign="slide" 
+							data-responsive_offset="off" 
+							data-responsive="off"
+							data-frames='[{"from":"opacity:0;","speed":1500,"to":"o:1;","delay":150,"ease":"Power2.easeInOut"},{"delay":"wait","speed":1500,"to":"opacity:0;","ease":"Power2.easeInOut"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 11;text-transform:left;background-color:rgba(18, 12, 20, 0.75);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 8 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-3" 
+							 id="slide-2979-layer-4" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-178','-178','-168','-141']" 
+										data-width="1"
+							data-height="100"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power1.easeIn"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 12;text-transform:left;background-color:rgb(252, 169, 1);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 9 -->
+						<div class="tp-caption Creative-SubTitle   tp-resizeme rs-parallaxlevel-2" 
+							 id="slide-2979-layer-3" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-91','-91','-81','-64']" 
+										data-fontsize="['14','14','14','12']"
+							data-lineheight="['14','14','14','12']"
+							data-width="none"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="text" 
+							data-responsive_offset="on" 
+
+							data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":2350,"ease":"Power3.easeOut"},{"delay":"wait","speed":1000,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['center','center','center','center']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 13; white-space: nowrap;text-transform:left; font-family: 'Trirong', serif;">WE PROVIDE A LOGISTICS FOR OUR CUSTOMERS GOODS</div>
+
+						<!-- LAYER NR. 10 -->
+						<div class="tp-caption Creative-Title   tp-resizeme rs-parallaxlevel-1" 
+							 id="slide-2979-layer-2" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-10','-10','-10','-10']" 
+										data-fontsize="['70','70','50','40']"
+							data-lineheight="['70','70','55','45']"
+							data-width="['none','none','none','320']"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="text" 
+							data-responsive_offset="on" 
+
+							data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":2550,"ease":"Power3.easeOut"},{"delay":"wait","speed":1000,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['center','center','center','center']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 14; white-space: nowrap;text-transform:left; font-family: 'Trirong', serif;">Secured<br/>Logistic</div>
+
+						<!-- LAYER NR. 11 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-3" 
+							 id="slide-2979-layer-5" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['137','137','119','100']" 
+										data-width="1"
+							data-height="100"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":2900,"ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"to":"y:[-100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power1.easeIn"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 15;text-transform:left;background-color:rgb(252, 169, 1);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 12 -->
+						<div class="tp-caption Creative-Button rev-btn  rs-parallaxlevel-15" 
+							 id="slide-2979-layer-6" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['top','top','top','top']" data-voffset="['694','611','689','540']" 
+										data-fontweight="['400','500','500','500']"
+							data-width="none"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="button" 
+							data-actions='[{"event":"click","action":"jumptoslide","slide":"next","delay":""}]'
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","speed":1500,"to":"o:1;","delay":3850,"ease":"Power2.easeOut"},{"delay":"wait","speed":500,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.75;sY:0.75;skX:0;skY:0;opacity:0;","ease":"Power1.easeIn"},{"frame":"hover","speed":"300","ease":"Power1.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgb(252, 169, 1);bc:rgb(252, 169, 1);bw:1px 1px 1px 1px;"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[15,15,15,15]"
+							data-paddingright="[50,50,50,50]"
+							data-paddingbottom="[15,15,15,15]"
+							data-paddingleft="[50,50,50,50]"
+
+							style="z-index: 16; white-space: nowrap;text-transform:left;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer; font-family: 'Trirong', serif;">CLICK TO NEXT </div>
+					</li>
+					<!-- SLIDE  -->
+					<li data-index="rs-2981" data-transition="fadethroughdark" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="2000"  data-thumb="images/slider/slider5.jpeg"  data-rotate="0"  data-saveperformance="off"  data-title="Delivery" data-param1="04" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+						<!-- MAIN IMAGE -->
+						<img src="images/slider/slider5.jpeg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="3" class="rev-slidebg" data-no-retina>
+						<!-- LAYERS -->
+
+						<!-- LAYER NR. 7 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-tobggroup" 
+							 id="slide-2979-layer-1" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']" 
+										data-width="full"
+							data-height="full"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-basealign="slide" 
+							data-responsive_offset="off" 
+							data-responsive="off"
+							data-frames='[{"from":"opacity:0;","speed":1500,"to":"o:1;","delay":150,"ease":"Power2.easeInOut"},{"delay":"wait","speed":1500,"to":"opacity:0;","ease":"Power2.easeInOut"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 11;text-transform:left;background-color:rgba(18, 12, 20, 0.75);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 8 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-3" 
+							 id="slide-2979-layer-4" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-178','-178','-168','-141']" 
+										data-width="1"
+							data-height="100"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power1.easeIn"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 12;text-transform:left;background-color:rgb(252, 169, 1);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 9 -->
+						<div class="tp-caption Creative-SubTitle   tp-resizeme rs-parallaxlevel-2" 
+							 id="slide-2979-layer-3" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-91','-91','-81','-64']" 
+										data-fontsize="['14','14','14','12']"
+							data-lineheight="['14','14','14','12']"
+							data-width="none"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="text" 
+							data-responsive_offset="on" 
+
+							data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":2350,"ease":"Power3.easeOut"},{"delay":"wait","speed":1000,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['center','center','center','center']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 13; white-space: nowrap;text-transform:left; font-family: 'Trirong', serif;">WE ASSURE YOU QUALITY DELIVERY TO SPECIFIED LOCATIONS</div>
+
+						<!-- LAYER NR. 10 -->
+						<div class="tp-caption Creative-Title   tp-resizeme rs-parallaxlevel-1" 
+							 id="slide-2979-layer-2" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['-10','-10','-10','-10']" 
+										data-fontsize="['70','70','50','40']"
+							data-lineheight="['70','70','55','45']"
+							data-width="['none','none','none','320']"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="text" 
+							data-responsive_offset="on" 
+
+							data-frames='[{"from":"y:50px;opacity:0;","speed":1500,"to":"o:1;","delay":2550,"ease":"Power3.easeOut"},{"delay":"wait","speed":1000,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","ease":"Power3.easeInOut"}]'
+							data-textAlign="['center','center','center','center']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 14; white-space: nowrap;text-transform:left; font-family: 'Trirong', serif;">Secure<br/>Delivery</div>
+
+						<!-- LAYER NR. 11 -->
+						<div class="tp-caption tp-shape tp-shapewrapper  rs-parallaxlevel-3" 
+							 id="slide-2979-layer-5" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['middle','middle','middle','middle']" data-voffset="['137','137','119','100']" 
+										data-width="1"
+							data-height="100"
+							data-whitespace="nowrap"
+				 
+							data-type="shape" 
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":2900,"ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"to":"y:[-100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power1.easeIn"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[0,0,0,0]"
+							data-paddingright="[0,0,0,0]"
+							data-paddingbottom="[0,0,0,0]"
+							data-paddingleft="[0,0,0,0]"
+
+							style="z-index: 15;text-transform:left;background-color:rgb(252, 169, 1);border-color:rgba(0, 0, 0, 0);border-width:0px;"> </div>
+
+						<!-- LAYER NR. 12 -->
+						<div class="tp-caption Creative-Button rev-btn  rs-parallaxlevel-15" 
+							 id="slide-2979-layer-6" 
+							 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+							 data-y="['top','top','top','top']" data-voffset="['694','611','689','540']" 
+										data-fontweight="['400','500','500','500']"
+							data-width="none"
+							data-height="none"
+							data-whitespace="nowrap"
+				 
+							data-type="button" 
+							data-actions='[{"event":"click","action":"jumptoslide","slide":"next","delay":""}]'
+							data-responsive_offset="on" 
+							data-responsive="off"
+							data-frames='[{"from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","speed":1500,"to":"o:1;","delay":3850,"ease":"Power2.easeOut"},{"delay":"wait","speed":500,"to":"x:0;y:0;z:0;rX:0;rY:0;rZ:0;sX:0.75;sY:0.75;skX:0;skY:0;opacity:0;","ease":"Power1.easeIn"},{"frame":"hover","speed":"300","ease":"Power1.easeInOut","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgb(252, 169, 1);bc:rgb(252, 169, 1);bw:1px 1px 1px 1px;"}]'
+							data-textAlign="['left','left','left','left']"
+							data-paddingtop="[15,15,15,15]"
+							data-paddingright="[50,50,50,50]"
+							data-paddingbottom="[15,15,15,15]"
+							data-paddingleft="[50,50,50,50]"
+
+							style="z-index: 16; white-space: nowrap;text-transform:left;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer; font-family: 'Trirong', serif;">CLICK TO NEXT </div>
+					</li>
+					<!-- SLIDE  -->
+					<li data-index="rs-2982" data-transition="fadethroughdark" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="2000"  data-thumb="images/slider/slider3.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Warehousing" data-param1="05" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
 						<!-- MAIN IMAGE -->
 						<img src="images/slider/slider3.jpg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="3" class="rev-slidebg" data-no-retina>
 						<!-- LAYERS -->
